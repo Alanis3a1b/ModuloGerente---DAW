db28c3965a1b81fef6f7058bc4b0907c242b86b3
ALANIS ALESSANDRA ALVAREZ BAÑOS <2021AB601@unicaeselsalvador0.onmicrosoft.com>
2024-04-24 21:03:45 -0600

    Se ha terminado de modelar todas las tablas de la base de datos

--- ModuloGerente - DAW/Models/dulcesaborDbContext.cs
+++ ModuloGerente - DAW/Models/dulcesaborDbContext.cs
@@ -8,12 +8,34 @@ namespace ModuloGerente___DAW.Models
         {
 
         }
-
+        //Tablas pedidos en linea
         public DbSet<pedidos> pedidos { get; set; }
         public DbSet<detalles_pedidos> detalles_pedidos { get; set; }
         public DbSet<comentarios> comentarios { get; set; }
         public DbSet<direcciones> direcciones { get; set; }
         public DbSet<pagos> pagos { get; set; }
         public DbSet<usuarios> usuarios { get; set; }
+
+        //Tablas pedidos en restaurante
+        public DbSet<cargos> cargos { get; set; }
+        public DbSet<categorias> categorias { get; set; }
+        public DbSet<combos> combos { get; set; }
+        public DbSet<cuenta> cuenta { get; set; }
+        public DbSet<detalle_fac> detalle_fac { get; set; }
+        public DbSet<Detalles_pedidos> Detalles_pedidos { get; set; }
+        public DbSet<empleados> empleados { get; set; }
+        public DbSet<encabezado_fac> encabezado_fac { get; set; }
+        public DbSet<estados> estados { get; set; }
+        public DbSet<items_combo> items_combo { get; set; }
+        public DbSet<items_menu> items_menu { get; set; }
+        public DbSet<items_promo> items_promo { get; set; }
+        public DbSet<mesas> mesas { get; set; }
+        public DbSet<promociones> promociones { get; set; }
+
+
+
+
+
+
     }
 }
--- ModuloGerente - DAW/Models/pedidos.cs
+++ ModuloGerente - DAW/Models/pedidos.cs
@@ -1,7 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Specialized;
+using System.ComponentModel.DataAnnotations;
 
 namespace ModuloGerente___DAW.Models
 {
+    //Tablas de la parte de pedidos en línea
     public class pedidos
     {
         [Key]
@@ -68,5 +70,143 @@ namespace ModuloGerente___DAW.Models
 
     }
 
+    //Tablas de la parte de pedidos en el restaurante
+    public class cargos
+    {
+        public int id_cargos { get; set; }
+        public BinaryData cargo { get; set; }
 
+    }
+
+    public class combos
+    {
+        public int id_combo { get; set; }
+        public string descripcion { get; set; }
+        public decimal precio { get; set; }
+        public string imagen { get; set; }
+        public int id_decimal { get; set; }
+
+
+    }
+
+    public class categorias
+    {
+        public int id_categoria { get; set; }
+        public string categoria { get; set; }
+
+    }
+
+    public class cuenta
+    {
+        public int id_cuenta { get; set; }
+        public int id_mesa { get; set; }
+        public string nombre { get; set; }
+        public int cantidad_personas { get; set; }
+        public string estado_cuenta { get; set; }
+        public DateTime fecha_hora { get; set; }
+
+    }
+
+    public class detalle_fac
+    {
+        public int id_detallefac { get; set; }
+        public int id_factura { get; set; }
+        public int id_detallepedido { get; set; }
+        public decimal precio { get; set; }
+        public decimal total_plato { get; set; }
+        public int cantidad { get; set; }
+
+    }
+
+    //Esta es parecida específica para los pedidos del restaurante
+    public class Detalles_pedidos
+    {
+        public int id_detallecuenta { get; set; }
+        public int id_cuenta { get; set; }
+        public int id_plato { get; set; }
+        public int cantidad { get; set; }
+        public string estado { get; set; }
+        public string tipo_plato { get; set; }
+        public decimal precio { get; set; }
+
+    }
+
+    public class empleados
+    {
+        public int id_empleado { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string direccion { get; set; }
+        public int telefono { get; set; }
+        public string correo { get; set; }
+        public int id_cargo { get; set; }
+        public int id_estado { get; set; }
+
+
+    }
+
+    public class encabezado_fac
+    {
+        public int id_factura { get; set; }
+        public int id_pedido { get; set; }
+        public int id_cliente { get; set; }
+        public DateTime fecha_cobro { get; set; }
+        public decimal total_cobrado { get; set; }
+        public string metodo_pago { get; set; }
+
+    }
+
+    public class estados
+    {
+        public int id_estado { get; set; }
+        public string nombre { get; set; }
+        public string tipo_estado { get; set; }
+    }
+
+    public class items_combo
+    {
+        public int id_combo { get; set; }
+        public int id_item_menu { get; set; }
+    }
+
+    public class items_menu
+    {
+        public int id_item_menu { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public decimal precio { get; set; }
+        public string imagen { get; set; }
+        public int id_estado { get; set; }
+        public int id_categoria { get; set; }
+
+    }
+
+    public class items_promo
+    {
+        public int id_promo { get; set; }
+        public int id_item_menu { get; set; }
+
+    }
+
+    public class mesas
+    {
+        public int id_mesa { get; set; }
+        public int cantidad_personas { get; set; }
+        public int id_estado { get; set; }
+        public string nombre_mesa { get; set; }
+
+    }
+
+    public class promociones
+    {
+        public int id_promo { get; set; }
+        public string descripcion { get; set; }
+        public decimal precio { get; set; }
+        public DateTime fecha_inicio { get; set; }
+        public decimal fecha_final { get; set; }
+        public string imagen { get; set; }
+        public int id_estado { get; set; }
+        public string nombre { get; set; }
+
+    }
 }
